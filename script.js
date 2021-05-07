8e89e85e76cd125ad76ae153ee948d15e180b74d
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-07 18:59:40 +0100

    add part of the code for Event propagation lecture part

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ btnScrollTo.addEventListener("click", function (e) {
 const h1 = document.querySelector("h1");
 
 const alertH1 = function (e) {
-  alert("hohoh");
+  alert("Welcome alert");
 };
 
 h1.addEventListener("mouseenter", alertH1);
@@ -92,3 +92,28 @@ setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
 // h1.onmouseenter = function (e) {
 //   alert("hohoh");
 // };
+
+////Event propagation lecture
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+// console.log(randomColor(0, 255));
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.currentTarget === this);
+  //Stop propagation
+  // e.stopPropagation();
+});
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector(".nav").addEventListener(
+  "click",
+  function (e) {
+    this.style.backgroundColor = randomColor();
+  },
+  true
+);
