1d1fb2b553db36e4bd7df385dcce470527cc772e
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-07 14:22:09 +0100

    add 3 types of Event Handlers

--- script.js
+++ script.js
@@ -78,3 +78,17 @@ btnScrollTo.addEventListener("click", function (e) {
   ////new way
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+const h1 = document.querySelector("h1");
+
+const alertH1 = function (e) {
+  alert("hohoh");
+};
+
+h1.addEventListener("mouseenter", alertH1);
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+
+////same older version
+// h1.onmouseenter = function (e) {
+//   alert("hohoh");
+// };
