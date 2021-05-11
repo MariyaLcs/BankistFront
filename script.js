92d799714d93b690c96aea614b1f6e5aefccdc1a
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-11 21:29:18 +0100

    add section for Building a Tabbed Component

--- script.js
+++ script.js
@@ -144,11 +144,31 @@ console.log(h1.childNodes);
 h1.firstElementChild.style.color = "white";
 h1.lastElementChild.style.color = "black";
 //Going upwards:parents
-console.log(h1.parentNode);
+// console.log(h1.parentNode);
 // h1.closest("h1").style.background = "var (--gradient-secondary)";
 //Going sideways: siblings
-console.log(h1.previousElementSibling);
+// console.log(h1.previousElementSibling);
 
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = "scale(0.9)";
+});
+
+//Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  //Guard clause
+  if (!clicked) return;
+  //Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+  //Active tab
+  clicked.classList.add("operations__tab--active");
+  //Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
