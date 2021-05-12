48bbcde91742ac380c3b1aa1846eb2501892f02d
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-12 21:25:33 +0100

    add code for the section - Implementing a Sticky Navigation: The Scroll Event

--- script.js
+++ script.js
@@ -189,3 +189,11 @@ const handleHover = function (e) {
 //Passing "argument" into handler
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+//Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener("scroll", function (e) {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
